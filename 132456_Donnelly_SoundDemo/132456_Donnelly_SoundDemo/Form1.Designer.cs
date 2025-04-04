69aa49be80daeea279952ebe31dd127a025b1d54
tdonnelly1102-nsd <112578536+tdonnelly1102-nsd@users.noreply.github.com>
2025-04-04 15:14:55 -0700

    trying to commit the sound object

---
+++ 132456_Donnelly_SoundDemo/132456_Donnelly_SoundDemo/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace _132456_Donnelly_SoundDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.buttonSoundEffect = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBoxBackGroundMusic = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(51, 49);
+            this.buttonPlay.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(224, 48);
+            this.buttonPlay.TabIndex = 0;
+            this.buttonPlay.Text = "Play Jet Effect";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // buttonSoundEffect
+            // 
+            this.buttonSoundEffect.Location = new System.Drawing.Point(51, 124);
+            this.buttonSoundEffect.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonSoundEffect.Name = "buttonSoundEffect";
+            this.buttonSoundEffect.Size = new System.Drawing.Size(224, 48);
+            this.buttonSoundEffect.TabIndex = 1;
+            this.buttonSoundEffect.Text = "Play Bubble Effect";
+            this.buttonSoundEffect.UseVisualStyleBackColor = true;
+            this.buttonSoundEffect.Click += new System.EventHandler(this.buttonSoundEffect_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(51, 197);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBoxBackGroundMusic
+            // 
+            this.comboBoxBackGroundMusic.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxBackGroundMusic.FormattingEnabled = true;
+            this.comboBoxBackGroundMusic.Items.AddRange(new object[] {
+            "Action Hero",
+            "Lost in the Forest",
+            "Raising Me Higher",
+            "Steel Rods",
+            "Storm Front",
+            "Night Music"});
+            this.comboBoxBackGroundMusic.Location = new System.Drawing.Point(341, 62);
+            this.comboBoxBackGroundMusic.Name = "comboBoxBackGroundMusic";
+            this.comboBoxBackGroundMusic.Size = new System.Drawing.Size(215, 24);
+            this.comboBoxBackGroundMusic.TabIndex = 3;
+            this.comboBoxBackGroundMusic.SelectedIndexChanged += new System.EventHandler(this.comboBoxBackGroundMusic_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 394);
+            this.Controls.Add(this.comboBoxBackGroundMusic);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonSoundEffect);
+            this.Controls.Add(this.buttonPlay);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Play Sounds";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Button buttonSoundEffect;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBoxBackGroundMusic;
+    }
+}
+
---
+++ 132456_Donnelly_SoundDemo/132456_Donnelly_SoundDemo/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+using WMPLib;
+using System.Reflection;
+
+
+namespace _132456_Donnelly_SoundDemo
+{
+    public partial class Form1 : Form
+    {
+        Sound playSounds = new Sound();
+        //WindowsMediaPlayer player = new WindowsMediaPlayer();
+
+        //List<byte[]> resources = new List<byte[]>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //resources.Add(Properties.Resources.Action_Hero);
+            //resources.Add(Properties.Resources.Lost_In_The_Forest);
+            //resources.Add(Properties.Resources.mixkit_raising_me_higher_34);
+            //resources.Add(Properties.Resources.Steel_Rods);
+            //resources.Add(Properties.Resources.Stormfront);
+
+            //byte[] b = Properties.Resources.mixkit_raising_me_higher_34;
+            //byte[] b = Properties.Resources.Action_Hero;
+            //byte[] b = Properties.Resources.Paranoia;
+            //FileInfo fileInfo = new FileInfo("test.wma");
+            //FileStream fs = fileInfo.OpenWrite();
+            //fs.Write(b, 0, b.Length);
+            //fs.Close();
+            comboBoxBackGroundMusic.SelectedIndex = 0;
+            //player.URL = getBackgroundMusicName(resources[0],comboBoxBackGroundMusic.Text);// fileInfo.Name;
+            //player.controls.stop();
+            playSounds.setBackgroundMusic(comboBoxBackGroundMusic.SelectedIndex, comboBoxBackGroundMusic.Text);
+        }
+
+        //public string getBackgroundMusicName(byte[] bm,string fn)
+        //{
+            
+        //    byte[] b = bm;
+        //    FileInfo fileInfo = new FileInfo(fn + "_td.wma");
+
+        //    FileStream fs = fileInfo.OpenWrite();
+        //    fs.Write(b, 0, b.Length);
+        //    fs.Close();
+
+        //    return fileInfo.Name;
+        //}
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            //SoundPlayer player = new SoundPlayer();
+            //player.SoundLocation = AppDomain.CurrentDomain.BaseDirectory + "\\1-16 We Will Rock You.wav";
+            //player.Play();
+            playSounds.PlayWeWillRockYou();
+        }
+
+        private void buttonSoundEffect_Click(object sender, EventArgs e)
+        {
+            //SoundPlayer player = new SoundPlayer();
+            //player.SoundLocation = AppDomain.CurrentDomain.BaseDirectory + "\\Door Creak and Scream.wav";
+            //player.Play();
+            playSounds.PlayCreakingDoor();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //playSounds.PlayHeartBeat();
+
+            
+            //player 
+            if(button1.Text == "Play")
+            {
+                button1.Text = "Stop";
+                //byte[] b = Properties.Resources.Steel_Rods;
+                //FileInfo fileInfo = new FileInfo("test.wma");
+                //FileStream fs = fileInfo.OpenWrite();
+                //fs.Write(b, 0, b.Length);
+                //fs.Close();
+                //player.URL = fileInfo.Name;
+                //player.controls.play();
+                playSounds.playBackground();
+            }
+            else
+            {
+                button1.Text = "Play";
+                //player.controls.stop();
+                playSounds.stopBackground();
+            }
+            
+        }
+
+        private void comboBoxBackGroundMusic_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBoxBackGroundMusic.SelectedIndex != -1)
+            {
+                button1.Text = "Play";
+                playSounds.stopBackground();
+                playSounds.setBackgroundMusic(comboBoxBackGroundMusic.SelectedIndex, comboBoxBackGroundMusic.Text);
+            }
+            
+            //player.controls.stop();
+            //player.URL = getBackgroundMusicName(resources[comboBoxBackGroundMusic.SelectedIndex],comboBoxBackGroundMusic.Text);// fileInfo.Name;
+        }
+    }
+}
