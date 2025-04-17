27c7c705c1095b90315224671f59b01da398845b
David Stall <davidstall312@gmail.com>
2025-04-17 14:50:34 -0700

    Added GameControl class

---
+++ Hunt the Wumpus 2025/GameControl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hunt_the_Wumpus_2025
+{
+    public class GameControl : IGameControl
+    {
+        public string BringHomeBacon()
+        {
+            return "";
+        }
+    }
+    public interface IGameControl
+    {
+        string BringHomeBacon(); // this brings home the bacon
+
+        void Maxim();
+    }
+}
