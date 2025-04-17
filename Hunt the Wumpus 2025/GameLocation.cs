9d74a2238fd22dcd194d39e9119e0d6d0da38f4a
jofi22 <azeemrobertoegizi@gmail.com>
2025-04-17 15:14:43 -0700

    Added interface

---
+++ Hunt the Wumpus 2025/GameLocation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hunt_the_Wumpus_2025
+{
+    public class GameLocation : IGameLocation
+    {
+
+    }
+    public interface IGameLocation
+    {
+
+    }
+}
