c9879d3f2f869fb17fe4b296fabf7ff6182faa43
Skele <Camilla_Mejia@outlook.com>
2025-04-17 19:09:01 -0700

    I added the trivia manager class

---
+++ Hunt the Wumpus 2025/TriviaManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Random; 
+
+namespace Hunt_the_Wumpus_2025
+{
+    internal class TriviaManager
+    {
+        private List<Trivia> TriviaList { get; set; }
+
+        TriviaManager() 
+        {
+            this.Deserialize();
+        }
+
+        public Trivia getTriviaData(int i)
+        {
+            return this.TriviaList[i];
+        }
+
+        public string getQuestion(int i)
+        {
+            return this.TriviaList[i].Question;
+        }
+        public string[] getPosssibleAnswers(int i)
+        {
+            return this.TriviaList[i].PosssibleAnswers;
+        }
+        public string getCorrectAnswer(int i)
+        {
+            return this.TriviaList[i].CorrectAnswer;
+        }
+
+        public class Trivia // pretty sure i need to define the class in this class to get the json to work. I will delete this if i find out its useless
+        {
+            public string Question { get; set; }
+            public string[] PossibleAnswers { get; set; } 
+            public string CorrectAnswer { get; set; }
+        }
+
+        private void Serialize()
+        {
+            string filePath = "TriviaQuestions.json";
+            try
+            {
+                string jsonString = JsonSerializer.Serialize(this.TriviaList);
+                File.WriteAllText(filePath, jsonString);
+                Console.WriteLine($"List successfully serialized and saved to {filePath}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred during serialization: {ex.Message}");
+            }
+
+        }
+        private void Deserialize() 
+        {
+            string filePath = "TriviaQuestions.json";
+            try
+            {
+                string jsonString = File.ReadAllText(filePath);
+                this.TriviaList = JsonSerializer.Deserialize<List<Trivia>> TriviaList(jsonString);
+                Console.WriteLine("List successfully deserialized:");
+                foreach (var item in deserializedList)
+                {
+                    Console.WriteLine($"- {item}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred during deserialization: {ex.Message}");
+            }
+        }
+    }
+}
