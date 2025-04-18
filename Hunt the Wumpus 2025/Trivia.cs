a3668579f7ec13b951298d3413819404dad3d5b1
Skele <Camilla_Mejia@outlook.com>
2025-04-17 18:08:45 -0700

    I put code the class :)). This is just going to be a constructor for the TriviaManager class that I will add in a minute.

---
+++ Hunt the Wumpus 2025/Trivia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hunt_the_Wumpus_2025
+{
+    internal class Trivia
+    {
+        public string Question { get; set; }
+        public string[] PossibleAnswers { get; set; }
+        public  string CorrectAnswer { get; set; }
+
+        public Trivia() { }
+
+        public Trivia(string question, string[] possibleAnswer, string correctAnswer)
+        {
+            this.Question = question;
+            this.PossibleAnswers = possibleAnswer;
+            this.CorrectAnswer = correctAnswer;
+        }
+    }
+}
