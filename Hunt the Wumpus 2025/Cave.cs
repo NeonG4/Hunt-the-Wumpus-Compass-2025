7629a2a5f19982e72db71476136206b216e11406
Nathan Choy <2030638@apps.nsd.org>
2025-04-17 18:41:16 -0700

    added 5 cave layouts (which directions are valid for each room)

--- Hunt the Wumpus 2025/Cave.cs
+++ Hunt the Wumpus 2025/Cave.cs
@@ -24,6 +24,13 @@ namespace Hunt_the_Wumpus_2025
 
             return true;
         }
+
+        public int[] GetReachableNeighboringRooms(int room, int caveIndex)
+        {
+            //placeholder
+
+            return [0, 0, 0];
+        }
     }
 
     public interface ICave
@@ -35,5 +42,7 @@ namespace Hunt_the_Wumpus_2025
         //To be called by game control to see if moving in specified direction is valid
 
         bool IsValidMove(int room, int direction, int caveIndex);
+
+        int[] GetReachableNeighboringRooms(int room, int caveIndex);
     }
 }
