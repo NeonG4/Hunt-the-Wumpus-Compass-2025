735d0a6132452ac5e193fe55f81efa666ff2c3b5
cirrus20 <nathanchoy18@gmail.com>
2025-04-21 14:49:25 -0700

    cave ui updated

--- Hunt the Wumpus 2025/TriviaManager.cs
+++ Hunt the Wumpus 2025/TriviaManager.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.Json;
-using System.Random; 
+using static System.Random; 
 
 namespace Hunt_the_Wumpus_2025
 {
