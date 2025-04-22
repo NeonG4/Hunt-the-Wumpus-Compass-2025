3381c821614fad40b166621e73d83f235b389987
David Stall <davidstall312@gmail.com>
2025-04-22 14:30:27 -0700

    Added UI Class Library

--- UI Class Library/UIClass.cs
+++ UI Class Library/UIClass.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Hunt_the_Wumpus_2025
 {
@@ -11,6 +12,7 @@ namespace Hunt_the_Wumpus_2025
     }
     public interface IUIClass
     {
-        
+        public void Render(PaintEventArgs e); // requires PaintEventArgs to draw to the win form
+        public void RenderUI(PaintEventArgs e); // requires PaintEventArgs to draw to the win form
     }
 }
