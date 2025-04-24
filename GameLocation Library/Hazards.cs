e5af9471d150ba7792b6d908d0a0ee3c35ca16d5
jofi22 <azeemrobertoegizi@gmail.com>
2025-04-24 15:13:31 -0700

    Added hazard generation

---
+++ GameLocation Library/Hazards.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hunt_the_Wumpus_2025
+{
+    public class Hazards
+    {
+        public string hazard;
+        public int room;
+
+        public Hazards(string h, int r)
+        {
+            hazard = h;
+            room = r;
+        }
+
+        public override string ToString()
+        {
+            return hazard;
+        }
+    }
+}
