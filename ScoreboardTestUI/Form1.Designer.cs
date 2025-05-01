9d6d5d3b5bf90e505f9400002a5c4ee95e7a84be
delyagamax <maxim_d@outlook.com>
2025-05-01 14:45:30 -0700

    Tests and stubs

--- ScoreboardTestUI/Form1.Designer.cs
+++ ScoreboardTestUI/Form1.Designer.cs
@@ -32,7 +32,11 @@
             scorebox = new ListBox();
             label1 = new Label();
             Addnewscore = new Button();
-            Scorename = new TextBox();
+            CaveNames = new TextBox();
+            scoretext = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            Playername = new TextBox();
             SuspendLayout();
             // 
             // button1
@@ -59,9 +63,9 @@
             label1.AutoSize = true;
             label1.Location = new Point(105, 270);
             label1.Name = "label1";
-            label1.Size = new Size(66, 15);
+            label1.Size = new Size(57, 15);
             label1.TabIndex = 2;
-            label1.Text = "Scorename";
+            label1.Text = "CaveType";
             // 
             // Addnewscore
             // 
@@ -73,19 +77,55 @@
             Addnewscore.UseVisualStyleBackColor = true;
             Addnewscore.Click += Addnewscore_Click;
             // 
-            // Scorename
+            // CaveNames
             // 
-            Scorename.Location = new Point(208, 262);
-            Scorename.Name = "Scorename";
-            Scorename.Size = new Size(100, 23);
-            Scorename.TabIndex = 4;
+            CaveNames.Location = new Point(208, 262);
+            CaveNames.Name = "CaveNames";
+            CaveNames.Size = new Size(100, 23);
+            CaveNames.TabIndex = 4;
+            // 
+            // scoretext
+            // 
+            scoretext.Location = new Point(208, 303);
+            scoretext.Name = "scoretext";
+            scoretext.Size = new Size(100, 23);
+            scoretext.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(105, 311);
+            label2.Name = "label2";
+            label2.Size = new Size(36, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Score";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(105, 234);
+            label3.Name = "label3";
+            label3.Size = new Size(39, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Name";
+            // 
+            // Playername
+            // 
+            Playername.Location = new Point(208, 226);
+            Playername.Name = "Playername";
+            Playername.Size = new Size(100, 23);
+            Playername.TabIndex = 8;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(Scorename);
+            Controls.Add(Playername);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(scoretext);
+            Controls.Add(CaveNames);
             Controls.Add(Addnewscore);
             Controls.Add(label1);
             Controls.Add(scorebox);
@@ -103,6 +143,10 @@
         private ListBox scorebox;
         private Label label1;
         private Button Addnewscore;
-        private TextBox Scorename;
+        private TextBox CaveNames;
+        private TextBox scoretext;
+        private Label label2;
+        private Label label3;
+        private TextBox Playername;
     }
 }
--- ScoreboardTestUI/Form1.cs
+++ ScoreboardTestUI/Form1.cs
@@ -26,7 +26,7 @@ namespace ScoreboardTestUI
 
         private void Addnewscore_Click(object sender, EventArgs e)
         {
-
+            
         }
     }
 }
