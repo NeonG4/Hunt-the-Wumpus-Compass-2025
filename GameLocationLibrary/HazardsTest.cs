048c41ce157efe76e0bbfdebdce43549f1084964
jofi22 <azeemrobertoegizi@gmail.com>
2025-05-05 14:47:46 -0700

    Implemented updated code for hazrad generaiton Added method for spawning in the player initially Added unimplemented method for accessing the player location at any point

--- GameLocationLibrary/HazardsTest.cs
+++ GameLocationLibrary/HazardsTest.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Net.NetworkInformation;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -24,14 +25,15 @@ namespace GameLocationLibrary
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (hazards.Count > 4)
+            if (hazards.Count == 0)
             {
                 AddHazard("Pit");
                 AddHazard("Pit");
                 AddHazard("Bats");
                 AddHazard("Bats");
+                button1.Enabled = false;
             }
-            
+
         }
         private void AddHazard(string hazard)
         {
@@ -51,18 +53,45 @@ namespace GameLocationLibrary
                 if (tmp)
                 {
                     hazards.Add(newHazard);
-                    listBox1.Items.Add(newHazard);
+                    listBoxHazards.Items.Add(newHazard);
                     return;
                 }
             }
         }
-            
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if (listBox1.SelectedItem is Hazards selectedHazard)
+            if (listBoxHazards.SelectedItem is Hazards selectedHazard)
             {
-                label1.Text = selectedHazard.hazard.ToString();
+                textBoxData.Text = selectedHazard.hazard.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SpawnPlayer();
+            button2.Enabled = false;
+        }
+
+        public int SpawnPlayer()
+        {
+            while (true)
+            {
+                int startingroom = random.Next(0, 30);
+                bool tmp = true;
+                for (int i = 0; i < hazards.Count; i++)
+                {
+                    if (hazards[i].room == startingroom)
+                    {
+                        tmp = false;
+                        i = hazards.Count();
+                    }
+                }
+                if (tmp)
+                {
+                    textBoxData.Text = startingroom.ToString();
+                    return startingroom;
+                }
             }
         }
     }
