66fc1e5e2627024187927979e65570b2903ba609
jofi22 <azeemrobertoegizi@gmail.com>
2025-05-08 14:45:22 -0700

    Added system.text.json package for trivia

--- Trivia/TriviaManager.cs
+++ Trivia/TriviaManager.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Text.Json;
 
 namespace Hunt_the_Wumpus_2025
