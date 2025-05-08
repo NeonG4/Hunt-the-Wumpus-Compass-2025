aab146e5f57cb11337d4490cb710f73f19d89395
David Stall <davidstall312@gmail.com>
2025-05-08 15:14:11 -0700

    Added some rendering

--- UI Library/UIClassManager.cs
+++ UI Library/UIClassManager.cs
@@ -20,7 +20,7 @@ namespace UI_Class_Library
             // standard size of a google slide, same size that this game is
             width = 960;
             height = 540;
-            comfortaaCollection.AddFontFile("Comfortaa-Light.ttf");
+            comfortaaCollection.AddFontFile("Comfortaa-Light.ttf"); // adds the font file
             comfortaa = new Font((FontFamily)comfortaaCollection.Families[0], 41f);
         }
         
@@ -34,10 +34,35 @@ namespace UI_Class_Library
         }
         public void RenderMainMenu(PaintEventArgs e)
         {
-            e.Graphics.Clear(Color.FromArgb(76, 76, 76));
             SolidBrush textBrush = new SolidBrush(Color.FromArgb(255, 255, 255));
-            e.Graphics.DrawString("Hunt the Wumpus", comfortaa, textBrush, new Point(100, 100));
+            SolidBrush shapeBrush = new SolidBrush(Color.FromArgb(127, 127, 127));
+            e.Graphics.Clear(Color.FromArgb(76, 76, 76));
+            DebugOnlyRenderGridPoints(e, 20); // makes it easy to draw on the screen
             
+           e.Graphics.FillPolygon(shapeBrush, Hexagon(500, 100, 40, new Point(width / 2, 200)));
+            e.Graphics.DrawString("Hunt the Wumpus", comfortaa, textBrush, new Point(100, 100));
+        }
+        private Point[] Hexagon(int width, int height, int padding, Point position)
+        {
+            List<Point> points = new List<Point>();
+            int x = position.X;
+            int y = position.Y;
+            points.Add(new Point(width / 2 - x, y));
+            points.Add(new Point(width / 2 - x + padding, height / 2 - y));
+            points.Add(new Point(width / 2 + x - padding, height / 2 - y));
+            return points.ToArray();
+        }
+        private void DebugOnlyRenderGridPoints(PaintEventArgs e, int size)
+        {
+            SolidBrush b = new SolidBrush(Color.FromArgb(255, 0, 0));
+            for (int i = 0; i < width; i+= size)
+            {
+                for (int j = 0; j < height; j += size)
+                {
+                    Rectangle rect = new Rectangle(i - 1, j - 1, 2, 2);
+                    e.Graphics.FillRectangle(b, rect);
+                }
+            }
         }
         public bool[] GetInputs()
         {
