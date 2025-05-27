30363fd45a67f54857c948c0745f6d166f44b166
David Stall <davidstall312@gmail.com>
2025-05-27 15:01:36 -0700

    added sound class, changed references

---
+++ SoundLibrary/SoundManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace SoundLibrary
+{
+    public class SoundManager : ISoundManager
+    {
+        public void StartSound(string soundName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StopAllSounds()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public interface ISoundManager
+    {
+        public void StartSound(string soundName);
+        public void StopAllSounds();
+    }
+}
